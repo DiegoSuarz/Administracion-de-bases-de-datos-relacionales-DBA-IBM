f466f9cccaa078a75e4f731e9ba2c6c88e401e40
Diego Srz <1412612@utp.edu.pe>
2025-09-30 11:22:32 -0500

    enviando laboratorio 15

---
+++ 15_Automatización de tareas en MySQL mediante scripts de shell/sqlbackup.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# The above line tells the interpreter this code needs to be run as a shell script.
+
+# Set the database name to a variable. 
+DATABASE='sakila'
+
+# This will be printed on to the screen. In the case of cron job, it will be printed to the logs.
+echo "Pulling Database: This may take a few minutes"
+
+# Set the folder where the database backup will be stored
+# backupfolder=./backups
+backupfolder=/mnt/c/Users/'dg_su/OneDrive - Universidad Tecnologica del Peru'/Estudios/Cursos/'Analisis de Datos'/DATABASE/'Administración de bases de datos relacionales (DBA) IBM'/code/'15_Automatización de tareas en MySQL mediante scripts de shell'/backups
+# Number of days to store the backup 
+keep_day=30
+
+sqlfile=$backupfolder/all-database-$(date +%d-%m-%Y_%H-%M-%S).sql
+zipfile=$backupfolder/all-database-$(date +%d-%m-%Y_%H-%M-%S).gz
+
+# Create a backup
+
+if mysqldump "$DATABASE" > "$sqlfile" ; then
+   echo 'Sql dump created'
+    # Compress backup 
+    if gzip -c "$sqlfile" > "$zipfile"; then
+        echo "The backup was successfully compressed  $(date)"
+    else
+        echo "Error compressing backupBackup was not created!  $(date)"
+        exit
+    fi
+    rm "$sqlfile" 
+else
+   echo "mysqldump return non-zero code No backup was created!  $(date)" 
+   exit
+fi
+
+# Delete old backups 
+find "$backupfolder" -mtime +$keep_day -delete
